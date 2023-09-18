8584d30682d4999f2f433668c336e57b58178967
L-Ryland <saintleso@gmail.com>
2023-09-18 17:31:53 +0800

    husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import ReactLogo from "./assets/react.svg";
 import "./App.css";
-import { Header, Login } from "@components/";
+import { Header } from "@components/";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import queryClient from "@utils/queryClient";
 
 function App() {
   const [count, setCount] = useState(0);
   return (
     <QueryClientProvider client={queryClient}>
-      <Header user={{name: "miao"}}/>
+      <Header user={{ name: "miao" }} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
